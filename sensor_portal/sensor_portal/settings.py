1e37eda8b8e8065dbcad42fe98542cf496755f06
jevansbio <32939196+jevansbio@users.noreply.github.com>
2025-05-27 14:11:55 +0200

    Correct periodic task times

--- sensor_portal/sensor_portal/settings.py
+++ sensor_portal/sensor_portal/settings.py
@@ -301,16 +301,16 @@ CELERY_ENABLE_UTC = False
 CELERY_BEAT_SCHEDULE = {
     "check_deployment_active": {
         "task": "data_models.tasks.check_deployment_active",
-        "schedule": crontab(minute="0"),
+        "schedule": crontab(minute="0", hour="*"),
     },
     "check_device_status": {
         "task": "data_models.tasks.check_device_status",
-        "schedule": crontab(hour="12",
+        "schedule": crontab(hour="12", minute="0",
                             day_of_week="mon-fri"),
     },
     "clean_local_storage": {
         "task": "data_models.tasks.clean_all_files",
-        "schedule": crontab(hour="1"),
+        "schedule": crontab(hour="1", minute="0"),
     },
 }
 
