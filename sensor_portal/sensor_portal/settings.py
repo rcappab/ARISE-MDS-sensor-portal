e119f9fb0829582f071629a47cb4b4d56780332e
jevansbio <32939196+jevansbio@users.noreply.github.com>
2025-05-27 18:53:20 +0200

    Fix settings

--- sensor_portal/sensor_portal/settings.py
+++ sensor_portal/sensor_portal/settings.py
@@ -52,7 +52,7 @@ ALLOWED_HOSTS = os.environ.get(
     'DJANGO_ALLOWED_HOSTS', "localhost 127.0.0.1 [::1]").split(" ")
 
 CSRF_TRUSTED_ORIGINS = os.environ.get(
-    'DJANGO_CRSF', "https://*.127.0.0.1").split(" ")
+    'DJANGO_CSRF', "https://*.127.0.0.1").split(" ")
 
 CSRF_COOKIE_DOMAIN = os.environ.get(
     'DJANGO_CSRF_COOKIE_DOMAIN', '127.0.0.1')
