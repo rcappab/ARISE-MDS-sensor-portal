61c215dc467ba2a282e8d07eee43feb850f853da
jevansbio <32939196+jevansbio@users.noreply.github.com>
2025-06-05 09:31:54 +0200

    Update models.py

--- sensor_portal/data_models/models.py
+++ sensor_portal/data_models/models.py
@@ -179,6 +179,12 @@ class Device(BaseModel):
 
     def deployment_from_date(self, dt):
 
+        print(
+            f"Attempt to find deployment for device {self.device_ID} for {dt}")
+
+        if dt is None:
+            return None
+
         # print(dt)
         all_deploys = self.deployments.all()
 
