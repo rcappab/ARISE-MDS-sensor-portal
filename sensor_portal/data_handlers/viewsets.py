f671828fe90c771624f51be78fd5453df114a81e
jevansbio <32939196+jevansbio@users.noreply.github.com>
2025-06-10 09:44:28 +0200

    Cache some viewsets

--- sensor_portal/data_handlers/viewsets.py
+++ sensor_portal/data_handlers/viewsets.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -11,11 +13,13 @@ class DataHandlerViewSet(viewsets.ViewSet):
     serializer_class = DataHandlerSerializer
     permission_classes = [IsAuthenticated]
 
+    @method_decorator(cache_page(60 * 60 * 2))
     def list(self, request):
         serializer = self.serializer_class(
             instance=settings.DATA_HANDLERS.data_handler_list, many=True)
         return Response(serializer.data)
 
+    @method_decorator(cache_page(60 * 60 * 2))
     def retrieve(self, request, pk=None):
         try:
             data_handler = settings.DATA_HANDLERS.data_handler_list[int(pk)]
